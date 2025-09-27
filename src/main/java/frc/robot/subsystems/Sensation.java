056f02cbd804d3107a2354575d72964dee35f3e8
asingharoy <bkharris5@cps.edu>
2025-09-27 15:38:42 -0500

    quick change

--- src/main/java/frc/robot/subsystems/Sensation.java
+++ src/main/java/frc/robot/subsystems/Sensation.java
@@ -57,7 +57,7 @@ public class Sensation extends SubsystemBase
     }
 
     public boolean coralExitedHopper() {
-        return !exitBeam.get() && hopperBackBeam.get() && hopperFrontBeam.get();
+        return !exitBeam.get() && /*hopperBackBeam.get() &&*/ hopperFrontBeam.get();
     }
 
     public double leftTOF(){
