21ec98909305d23c6a9c7727e7acf878a37b1293
BryceH127 <bkharris5@cps.edu>
2025-10-07 16:02:47 -0500

    Changed ChassisSpeed
    Changed it from 2.5 to .5

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -260,8 +260,8 @@ public class SwerveSubsystem extends SubsystemBase
   public Command driveToPose(Pose2d pose)
   {
 // Create the constraints to use while pathfinding
-    PathConstraints constraints = new PathConstraints(2.5
-        /*swerveDrive.getMaximumChassisVelocity()*/, 2.5,
+    PathConstraints constraints = new PathConstraints(.5
+        /*swerveDrive.getMaximumChassisVelocity()*/, .5,
         swerveDrive.getMaximumChassisAngularVelocity(), Units.degreesToRadians(720));
 
     Logger.recordOutput("Drive/CurrentCommand", "RunningDriveToPose");
