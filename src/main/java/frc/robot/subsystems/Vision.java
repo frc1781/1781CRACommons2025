712aa7eaff4ff8e76846d6903637090b2f7b30e2
asingharoy <asingharoy@cps.edu>
2025-11-13 17:05:21 -0600

    correct camera pose

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -360,12 +360,12 @@ public class Vision
     public enum Cameras
     {
       LEFT_CAM("LeftApriltag",
-          new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(-25), Units.degreesToRadians(-20)),
+          new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(25), Units.degreesToRadians(-20)),
           new Translation3d(Units.inchesToMeters(8.5), Units.inchesToMeters(8.5), Units.inchesToMeters(6.5)),
              VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1)),
 
     RIGHT_CAM("RightApriltag",
-              new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(-25), Units.degreesToRadians(20)),
+              new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(25), Units.degreesToRadians(20)),
               new Translation3d(Units.inchesToMeters(8.5), Units.inchesToMeters(-8.5), Units.inchesToMeters(6.5)),
               VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1));
   
